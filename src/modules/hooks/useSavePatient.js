a2c1f84c60eae95ebd9950e6b5ad6c3fa2bf593a
Ramin <raminxanifmuslim@gmail.com>
2023-06-21 10:45:58 +0400

    asdf

--- src/modules/hooks/useSavePatient.js
+++ src/modules/hooks/useSavePatient.js
@@ -106,7 +106,6 @@ function useSavePatient() {
     }
 
     const handleSave = async () => {
-        console.log('handleSave');
         setIsLoading(true)
         setTimeout(() => {
             setIsLoading(false)
